337c6be1fa4f686a1345342dc1b0e646c2fd85db
MohamedAbdel-wahed <webdeveloper.int99@gmail.com>
2020-12-07 15:27:19 +0200

    edited ws link

--- src/ApolloServiceProvider.js
+++ src/ApolloServiceProvider.js
@@ -26,7 +26,7 @@ const authLink = setContext((_, { headers }) => {
 httpLink= authLink.concat(httpLink)
 
 const wsLink = new WebSocketLink({
-  uri: `ws://https://pensa-chat.herokuapp.com/graphql`,
+  uri: `https://pensa-chat.herokuapp.com/graphql`,
   options: {
     reconnect: true,
     connectionParams: {
